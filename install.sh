4eeac2f6dd1d57cb8092c9314857a466e7e0c326
Richard van der Oost <richard@blendergrid.com>
2023-02-23 12:00:16 +0100

    Fix xcode error

--- install.sh
+++ install.sh
@@ -13,6 +13,8 @@ abort() {
   exit 1
 }
 
+# TODO: Use caffeinate to keep it awake
+
 # Fail fast with a concise message when not using bash
 if [ -z "${BASH_VERSION:-}" ]
 then
@@ -27,7 +29,7 @@ echo "Please authenticate"
 sudo -v
 while true; do
     sudo -n true
-    sleep 60
+    caffeinate -id sleep 60
     kill -0 "$$" || exit
 done 2>/dev/null &
 
@@ -36,12 +38,14 @@ sudo softwareupdate -ia --verbose
 
 : Install Xcode Command Line Tools
 if ! $(xcode-select -p &>/dev/null); then
-  xcode-select --install &>/dev/null
+    xcode-select --install &>/dev/null
 
-  : Wait until the Xcode Command Line Tools are installed
-  until $(xcode-select -p &>/dev/null); do
-    sleep 5
-  done
+    sudo xcode-select --switch /Library/Developer/CommandLineTools # Enable command line tools
+
+    : Wait until the Xcode Command Line Tools are installed
+    until $(xcode-select -p &>/dev/null); do
+        sleep 10
+    done
 fi
 
 : Accept the Xcode/iOS license agreement
@@ -79,3 +83,7 @@ cd "$BOOTSTRAP_DIR"
 cd "$HOME"
 
 # Download all dotfiles and link them?
+
+# Download more git repos
+
+# Set up cron jobs
